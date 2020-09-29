593b4681d5d562b7f6e4d70eaa3ce0b2d010fbd4
ainanitajriyan <ainani.tajriyan80@gmail.com>
2020-09-29 11:14:36 +0700

    Fixing bug Dark Mode & Navbar di menu Edit Profile & Change Password

--- i-blast/resources/views/password/edit.blade.php
+++ i-blast/resources/views/password/edit.blade.php
@@ -1,29 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-<script src="{{ asset('js/app.js') }}" defer></script>
-<script src="{{ asset('fontawesome/js/fontawesome.min.js') }}" defer></script>
-<script src="{{ asset('fontawesome/js/all.js') }}" defer></script>
-<script src="{{ asset('fontawesome/js/solid.js') }}" defer></script>
-<script src="{{ asset('fontawesome/js/brands.js') }}" defer></script>
-<script src="{{ asset('fontawesome/js/regular.js') }}" defer></script>
-<script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
-
-
-<!-- Fonts -->
-<link rel="dns-prefetch" href="//fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-<link rel="stylesheet" href="{{ asset('fontawesome/css/fontawesome.min.css') }}">
-<link rel="stylesheet" href="{{ asset('fontawesome/all.css') }}">
-<link rel="stylesheet" href="{{ asset('fontawesome/solid.css') }}">
-<link rel="stylesheet" href="{{ asset('fontawesome/brands.css') }}">
-<link rel="stylesheet" href="{{ asset('fontawesome/regular.css') }}">
-
-<!-- Styles -->
-<link href="{{ asset('css/app.css') }}" rel="stylesheet">
-<link rel="stylesheet" href="{{ asset('css/style.css') }}">
-<link rel="stylesheet" href="{{ asset('css/simple-sidebar.css') }}">
-
 <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
