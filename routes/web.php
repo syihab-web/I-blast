9158012adfc48d457ba30457c2a42126f2ea2a9d
ainanitajriyan <ainani.tajriyan80@gmail.com>
2020-11-02 12:33:45 +0700

    Revisi Routes

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/email/seeTemplate', function () {
 
 Auth::routes(['verify' => true]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'EmailController@dashboard');
 
 Route::group(['middleware' => 'auth'], function () {
 
